568f57dd0700f0e4f36eaefa86cafb9731b95d43
Anuraag-45 <koyalkaranurag45@gmail.com>
2025-02-18 20:53:01 +0000

    Final project updates

---
+++ client/app/components/Common/ComingSoon/index.js
@@ -0,0 +1,21 @@
+/**
+ *
+ * ComingSoon
+ *
+ */
+
+import React from 'react';
+
+const ComingSoon = props => {
+  return (
+    <div className='coming-soon'>
+      <h3>Coming soon</h3>
+      {props.children}
+    </div>
+  );
+};
+
+export default ComingSoon;
+
+
+// Potential improvement: Add error handling
