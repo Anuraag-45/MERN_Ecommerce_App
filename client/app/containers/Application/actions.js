cbf0beb511f60fc6932959b56dc113d91fed15e1
Anuraag-45 <koyalkaranurag45@gmail.com>
2025-02-18 21:36:22 +0000

    Code cleanup and optimization

---
+++ client/app/containers/Application/actions.js
@@ -0,0 +1,16 @@
+/*
+ *
+ * Application actions
+ *
+ */
+
+import { DEFAULT_ACTION } from './constants';
+
+export const defaultAction = () => {
+  return {
+    type: DEFAULT_ACTION
+  };
+};
+
+
+// Note: Could be optimized in future iterations
